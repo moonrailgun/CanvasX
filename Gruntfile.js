9284678e9c58586f4a63352ec4838f612dffc327
moonrailgun <moonrailgun@gmail.com>
2016-01-17 11:24:59 +0800

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/**
+ * Created by moonrailgun on 2016-01-16.
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/*/*.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify']);
+};
---
+++ src/manager/gameManager.js
@@ -0,0 +1,7 @@
+/**
+ * Created by moonrailgun on 2016-01-16.
+ */
+
+var GameManager = function(canvas, options){
+
+};
