43868ae962fa50f1bbfb23d1fee03c113408ddef
moonrailgun <moonrailgun@gmail.com>
2016-01-18 18:31:25 +0800

    基础对象
    ADD 基础对象

---
+++ src/base/gameObject.js
@@ -0,0 +1,13 @@
+/**
+ * Created by moonrailgun on 2016-01-17.
+ */
+
+var GameObject = function(context,opts){
+    var x = opts.x || 0;
+    var y = opts.y || 0;
+    var angle = opts.angle || 0;
+};
+
+GameObject.fn = GameObject.prototype = {
+
+};
---
+++ src/base/geometry.js
@@ -0,0 +1,7 @@
+/**
+ * Created by moonrailgun on 2016-01-17.
+ */
+
+var Geometry = function(context){
+
+};
